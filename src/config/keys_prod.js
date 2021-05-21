b749cd27f91f45221bfa146a8b0dab51ff2dadca
theo m <theo@mantz.io>
2021-05-21 19:21:38 -0400

    Updates

--- src/config/keys_prod.js
+++
@@ -1,6 +0,0 @@
-const keysProd = {
-  unsplashAccessKey: process.env.REACT_APP_UNSPLASH,
-  pexelsAPIKey: process.env.REACT_APP_PEXELS,
-};
-
-export default keysProd
--- src/config/photos.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  pexels: {
-    texturedArch: "796544"
-  },
-  unsplash: {
-    content_filter: "high",
-    topics: "wallpapers"
-  }
-}
